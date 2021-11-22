ea0b000d4a983f806d54ad644f7ec4a0b1ccac7a
Beetlebum <jojo@beetlebum.de>
2021-11-22 13:03:42 +0100

    RotaryCtrl added

---
+++ src/components/UI/RotaryCtrl.js
@@ -0,0 +1,9 @@
+const RotaryCtrl = (props) => {
+    return <form>
+        <input type="number" min="0" max="100" />
+        <button>CW</button>
+        <button>CCW</button>
+    </form>
+} 
+
+export default RotaryCtrl
