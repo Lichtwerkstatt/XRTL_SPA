2c4b364d2f36cde5419cea9ddde588ab8de3afcc
Beetlebum <jojo@beetlebum.de>
2021-11-22 12:59:57 +0100

    KM100 module added

---
+++ src/components/assembly/KM100.js
@@ -0,0 +1,8 @@
+const KM100 = (props) => {
+    return <div>
+        <h2>KM100</h2>
+        <p>id: {props.id}</p>
+    </div>
+}
+
+export default KM100
--- src/components/experiment/MichelsonInterferometer/MichelsonInterferometer.js
+++ src/components/experiment/MichelsonInterferometer/MichelsonInterferometer.js
@@ -1,3 +1,10 @@
-const MichelsonInterferometer = () => {}
+import KM100 from "../../assembly/KM100"
+
+const MichelsonInterferometer = () => { 
+    
+    return <div>
+        <h1>Michelson Interferometer</h1>
+        <KM100 id="1" />
+        </div>}
 
 export default MichelsonInterferometer
