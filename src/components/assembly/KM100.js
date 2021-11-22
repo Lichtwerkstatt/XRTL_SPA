45839f236df1803ed08f02b56d423127b95140d1
Beetlebum <jojo@beetlebum.de>
2021-11-22 13:04:14 +0100

    RotaryCtrl added

--- src/components/assembly/KM100.js
+++ src/components/assembly/KM100.js
@@ -1,7 +1,11 @@
+import RotaryCtrl from "../UI/RotaryCtrl"
+
 const KM100 = (props) => {
     return <div>
         <h2>KM100</h2>
         <p>id: {props.id}</p>
+        <RotaryCtrl />
+        <RotaryCtrl />
     </div>
 }
 
