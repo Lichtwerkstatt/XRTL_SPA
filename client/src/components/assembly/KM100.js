07c9f9b626a3c412296a9b93bfce110c00d2208b
Beetlebum <jojo@beetlebum.de>
2021-12-02 08:46:56 +0100

    finished preliminery component style

--- client/src/components/assembly/KM100.js
+++ client/src/components/assembly/KM100.js
@@ -15,22 +15,24 @@ const KM100 = (props) => {
       footer={footer}
       top={props.top}
       left={props.left}
+      height="250px"
+      width="230px"
     >
       <RotaryCtrl
         rotation={props.rotationTop}
         component={props.id}
         control="top"
         newStatus={handleChangeFooter}
-        top="5"
-        left="20"
+        top="20"
+        left="130"
       />
       <RotaryCtrl
         rotation={props.rotationBottom}
         component={props.id}
         control="bottom"
         newStatus={handleChangeFooter}
-        top="5"
-        left="80"
+        top="50"
+        left="130"
       />
     </Window>
   );
--- client/src/components/assembly/SM1ZP.js
+++ client/src/components/assembly/SM1ZP.js
@@ -1,11 +1,24 @@
-import RotaryCtrl from "../UI/RotaryCtrl"
-import Window from "../UI/Window"
+import RotaryCtrl from "../UI/RotaryCtrl";
+import Window from "../UI/Window";
 
 const SM1ZP = (props) => {
-    return <Window header={props.title+" ("+props.id+")"} top={props.top} left={props.left}>
-        <RotaryCtrl rotation={props.rotation} component={props.id} control="linStage" />
+  return (
+    <Window
+      header={props.title + " (" + props.id + ")"}
+      top={props.top}
+      left={props.left}
+      width="250px"
+      height="150px"
+    >
+      <RotaryCtrl
+        rotation={props.rotation}
+        component={props.id}
+        control="linStage"
+        top="35"
+        left="150"
+      />
     </Window>
-}
+  );
+};
 
-export default SM1ZP
-
+export default SM1ZP;
