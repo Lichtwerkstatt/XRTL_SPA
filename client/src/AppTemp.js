4720f228f56c4e496f2c616bb3367875a27e2e0f
Beetlebum <jojo@beetlebum.de>
2021-12-03 09:54:19 +0100

    got app running again :)

--- client/src/AppTemp.js
+++ client/src/AppTemp.js
@@ -1,10 +1,14 @@
 import MichelsonInterferometer from "./components/experiment/MichelsonInterferometer/MichelsonInterferometer"
 import NavBar from "./components/UI/NavBar"
+import Console from "./Console/Console"
+import Chat from "./Chat/Chat"
 
 const AppTemp = () => {
    return <>
     <NavBar />
     <MichelsonInterferometer />
+    <Console/>
+    <Chat/>
    </>
    
    
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Chat from "./Chat/Chat";
-import Console from "./Console/Console"
-
-import App from "./App";
 import MichelsonInterferometer from "./components/experiment/MichelsonInterferometer/MichelsonInterferometer"
 import AppTemp from "./AppTemp"
 
