8b186c75afc05e29f926d1d87912bb8567b3742f
Beetlebum <jojo@beetlebum.de>
2021-12-04 16:33:24 +0100

    draggable Windows added

--- client/src/components/UI/Window.js
+++ client/src/components/UI/Window.js
@@ -1,28 +1,43 @@
-import styles from "./Window.module.css";
+import Draggable from "react-draggable"
+import styles from "./Window.module.css"
 
 const Window = (props) => {
-  console.log("Rendering Window...");
+  console.log("Rendering Window...")
   return (
-    <div
-      class="window"
-      className={styles.window}
-      style={{ top: props.top + "px", left: props.left + "px" }}
-    >
-      <div class="window_header" className={styles.windowHeader}>
-        {props.header}
-      </div>
-      <div class="window_content" 
-        className={styles.windowContent} 
-        style={{height: props.height , width: props.width, background:"url("+props.background+")"}}>
-        {props.children}
-      </div>
-      {props.footer !== undefined && (
-        <div class="window_footer" className={styles.windowFooter}>
-          {props.footer}
+    <Draggable handle=".draggableHandler">
+      <div
+        className={styles.window}
+        style={{ top: props.top + "px", left: props.left + "px" }}
+      >
+        <div className={styles.windowHeader}>
+          <span
+            className="draggableHandler" //FIXME draggable doesnt seem to work with inline JSX classes. 
+            style={{
+              display: 'block',
+              width: "calc(100% - 50px)",
+              //border: "1px solid yellow",
+              cursor: "move",
+            }}
+          >
+            {props.header}
+          </span>
         </div>
-      )}
-    </div>
-  );
-};
+        <div
+          className={styles.windowContent}
+          style={{
+            height: props.height,
+            width: props.width,
+            background: "url(" + props.background + ")",
+          }}
+        >
+          {props.children}
+        </div>
+        {props.footer !== undefined && (
+          <div className={styles.windowFooter}>{props.footer}</div>
+        )}
+      </div>
+    </Draggable>
+  )
+}
 
-export default Window;
+export default Window
