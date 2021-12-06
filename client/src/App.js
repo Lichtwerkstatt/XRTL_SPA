9bec330a6254d42f0d444e59fe109ec2141b3b8c
Claralein <clara.henkel@uni-jena.de>
2021-12-06 12:16:49 +0100

    Mit funktionierendem Serververbindungsaufbau

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import {SocketContext, socket} from './services/socket';
 import Child from './Chat/Chat_old';
+import AppTemp from './AppTemp';
 import React from 'react';
 
 const App = () => {
   return (
     <SocketContext.Provider value={socket}>
-      <Child />
+      <AppTemp />
 
     </SocketContext.Provider>
   );
--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,11 +1,11 @@
 
-/* import io from 'socket.io-client'
-
-//export const socket = io.connect("http://192.168.4.1:7000")
-export const socket = io.connect("http://192.168.4.1:7000") */
-
-import socketio from "socket.io-client";
+import io from "socket.io-client";
 import React from "react";
 
-export const socket = socketio('"http://192.168.4.1:7000"',  {transports: ['websocket']});
+//export const socket = io.connect("http://192.168.4.1:7000");
+export const socket = io.connect("http://localhost:7000");
 export const SocketContext = React.createContext();
+
+
+
+
