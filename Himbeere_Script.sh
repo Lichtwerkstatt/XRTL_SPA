a021e82f660c7eee21396cca3db533552c3984ee
Claralein <clara.henkel@uni-jena.de>
2022-01-07 09:18:22 +0100

    Sudo vor Befehle gesetzt

--- Himbeere_Script.sh
+++ Himbeere_Script.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 echo Checking for new dependencies 
-cd "/home/pi/XRTL_SPA"
-npm install
 cd "/home/pi/XRTL_SPA/client"
-npm install
+sudo npm install
 cd "/home/pi/XRTL_SPA/server"
-npm install
+sudo npm install
 
 echo Done with checking & installing
-git checkout main
+sudo git checkout main
 echo Pull request
-git pull
+sudo git pull
 
 echo Starting server.js 
 cd "/home/pi/XRTL_SPA/server"
-node server_himbeere.js
+sudo node server_himbeere.js
 
