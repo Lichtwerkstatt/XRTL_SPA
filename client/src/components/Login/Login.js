7c666f6ff004e17b74709ce981d3666b305fc8bd
Claralein <clara.henkel@uni-jena.de>
2022-03-22 10:06:30 +0100

    Login work perfectly, error messages for missing username & custom address if chossen is displayed, missing connection to the icon in the nav bar & starting a connection to the server

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,39 +3,54 @@ import styles from "./Login.module.css"
 import { useAppContext } from "../../services/AppContext"
 import { BiFontColor } from 'react-icons/bi'
 
-
-//erst bei zweitem aufrufen wird Varibale belegt
 const Login = (props) => {
     const [username, setUsername] = useState("");
     const [fontColor, setfontColor] = useState("white");
     const [connection, setConnection] = useState('http://localhost:7000');
     const [customConnection, setCustomConnection] = useState('');
+    const [custom, setCuston] = useState(false)
     const appCtx = useAppContext();
 
+    //let custom = false;
+
     const displayCustom = () => {
-        console.log("hallo")
+        setCuston(true);
         var inputCustom = document.getElementById("customInput");
         var dropDownMenu = document.getElementById("dropDownMenu")
-        console.log(inputCustom);
-        console.log(dropDownMenu)
 
-        if (dropDownMenu.style.display == 'none') {
-            dropDownMenu.style.display = 'block';
-            inputCustom.style.display = 'none';
-        } else {
-            dropDownMenu.style.display = 'none';
-            inputCustom.style.display = 'block'
-        }
-
+        dropDownMenu.style.display = 'none';
+        inputCustom.style.display = 'block'
     }
 
-    /*  return (props.trigger) ? ( */
+    const loginCaseChecking = () => {
+        var errorLabel = document.getElementById('errorLabel');
+        var login = document.getElementById("login");
+
+        if (username == '') {
+            errorLabel.innerHTML = "Please enter a username!";
+        } else {
+            errorLabel.innerHTML = "";
+
+            if (custom == false) {
+                login.style.display = 'none'
+            } else if (custom == true && customConnection == "") {
+                errorLabel.innerHTML = "Please enter a server address!";
+            } else if (custom == true && customConnection != "") {
+                login.style.display = 'none'
+                errorLabel.innerHTML = "";
+            } else {
+                errorLabel.innerHTML = 'Something went wrong'
+            }
+        }
+    }
+
     return (
         <div className="login" id="login">
             <div className={styles.popupWindow}>
             </div>
             <div className={styles.popupInner}>
                 <h3 title="settings">Settings</h3>
+
                 <div className={styles.setUsername}>
                     <label >Username:</label>
                     <input
@@ -49,44 +64,45 @@ const Login = (props) => {
                         required
                     ></input>
                 </div>
+
                 <div className={styles.colorPicker}>
                     <li onClick={(e) => {
                         var c = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
                         setfontColor(c);
                     }}><BiFontColor size={33} color={fontColor} /></li>
                 </div>
+
                 <div className={styles.serverConnect}>
-                    <label for="serverOption" >
+                    <label htmlFor="serverOption" >
                         Choose server:
                     </label>
-                    <select id="dropDownMenu" name="serverOption"
+                    <select id="dropDownMenu" name="serverOption" data-testid="dropDownMenu"
                         onClick={(e) => {
                             setConnection(e.target.value);
                         }}>
-                        <option value='http://localhost:7000'>http://localhost:7000</option>
-                        <option value='http://192.168.1.42:7000'>http://192.168.1.42:7000</option>
-                        <option value='' onClick={displayCustom}>Define</option>
+                        <option data-testid='localhost' value='http://localhost:7000'>http://localhost:7000</option>
+                        <option data-testid='himbeere' value='http://192.168.1.42:7000'>http://192.168.1.42:7000</option>
+                        <option data-testid='custom' value='' onClick={displayCustom}>Define</option>
                     </select>
 
                     <input id="customInput"
                         placeholder="http://..."
                         value={customConnection}
-                        onChange={(e) => { setCustomConnection(e.target.value) }}
+                        onChange={(e) => {
+                            setCustomConnection(e.target.value)
+                        }}
                     />
                 </div>
 
+                <div className={styles.ErrorMessages}>
+                    <label title="errorLabel" id="errorLabel" color="white"></label>
+                </div>
                 <button title="login"
-                    onClick={(e) => {
-                        var login = document.getElementById("login");
-                        login.style.display = 'none';
-                    }}>
+                    onClick={loginCaseChecking}>
                     Login
                 </button>
-
             </div>
-        </div>
+        </div >
     );
-    /*  ) : ""; */
 }
-
 export default Login;
--- client/src/components/UI/NavBar.js
+++ client/src/components/UI/NavBar.js
@@ -3,7 +3,7 @@ import styles from "./NavBar.module.css"
 import { GiLaserWarning } from "react-icons/gi"
 import { ImConnection } from "react-icons/im"
 import { BsBox } from "react-icons/bs"
-import {FaTags} from "react-icons/fa"
+import { FaTags } from "react-icons/fa"
 import { MdOutlineScreenRotation } from "react-icons/md"
 import { useSocketContext } from '../../services/SocketContext'
 import { useAppContext } from '../../services/AppContext'
@@ -13,13 +13,12 @@ const NavBar = () => {
     const appCtx = useAppContext();
     const socketCtx = useSocketContext();
 
-
     let connectionStatusColor = ""
-    if (socketCtx.connected) {connectionStatusColor="white"}
+    if (socketCtx.connected) { connectionStatusColor = "white" }
     let autoRotateColor = ""
-    if (appCtx.autoRotate) {autoRotateColor="white"}
+    if (appCtx.autoRotate) { autoRotateColor = "white" }
     let showTagsColor = ""
-    if (appCtx.showTags) {showTagsColor = "white"}
+    if (appCtx.showTags) { showTagsColor = "white" }
     let cameraStatusColor = "";
     if (appCtx.showWebcam) { cameraStatusColor = "white" }
 
@@ -32,7 +31,7 @@ const NavBar = () => {
                 <li onClick={appCtx.toggleShowVirtualLayer}><BsBox size={26} /></li>
                 <li><GiLaserWarning size={29} /></li>
 
-                <li onClick={appCtx.toggleAutoRotate}><MdOutlineScreenRotation size={26} color={autoRotateColor}/></li>
+                <li onClick={appCtx.toggleAutoRotate}><MdOutlineScreenRotation size={26} color={autoRotateColor} /></li>
                 <li onClick={appCtx.toggleShowTags}><FaTags size={25} color={showTagsColor} /></li>
 
                 <li onClick={appCtx.toggleShowWebcam}><BsCamera size={26}
