c877e848e569a2f1e3ab64251d79025c8f0b0665
Claralein <clara.henkel@uni-jena.de>
2022-04-04 14:20:54 +0200

    First version of the Server GUI

--- server/KivyGUI/GUI/hello.py
+++ server/KivyGUI/GUI/hello.py
@@ -1,34 +1,11 @@
 from cgitb import text
 import kivy;
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.switch import Switch
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
 
-
-class Grid(GridLayout):
-    def __init__(self, **kwargs):
-
-        super(Grid, self).__init__(**kwargs)
-        self.cols = 2
-        self.row_force_default=True
-        self.row_default_height=120
-        self.col_force_default=True
-        self.col_default_height=100
-
-
-        #Switch for Server start and shutdown
-        self.add_widget(Label(text="Serverstatus", font_size=22))
-        self.settings_sample = Switch(active=False)
-
-        self.add_widget(self.settings_sample)
-        self.settings_sample.bind(active=switch_callback)      
-
-        self.add_widget(Button(text="Init"))
-        self.add_widget(Button(text="Reset"))
-
+class Grid(Widget):
+    pass
  
 #1824x984 Auflösung des Raspberry Pis
 
@@ -36,9 +13,11 @@ def switch_callback(switchObject, switchValue):
 
     print('Value of sample settings is:', switchValue) 
 
-class App(App):
+class ServerApp(App):
     def build(self):
+        Window.clearcolor = (0, 0, 0, 0.6)
+        Window.size = (1080, 720)
         return Grid()
 
 if __name__ == '__main__':
-    App().run()
+    ServerApp().run()
