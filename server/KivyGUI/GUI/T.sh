01c0c319130002ca4c59f9c0bd5c042edf3a3cfd
Claralein <93766288+Claralein@users.noreply.github.com>
2022-06-10 12:52:29 +0200

    Update T.sh

--- server/KivyGUI/GUI/T.sh
+++ server/KivyGUI/GUI/T.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$(tput setaf 3)Start installing all the pip dependencies$(tput setaf 7)"
-pip install kivy[full] python-socketio kivymd virtualenv
+pip install kivy[full] KivyGradient python-socketio kivymd virtualenv
 
 echo "$(tput setaf 2)Done with the installation!$(tput setaf 7)"
