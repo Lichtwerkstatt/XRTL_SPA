40ff3b8206a0c487f229abe627271d3ebed3dce5
Claralein <clara.henkel@uni-jena.de>
2022-06-30 12:48:33 +0200

    Switch Style

--- client/src/components/UI/SwitchOnOff.js
+++ client/src/components/UI/SwitchOnOff.js
@@ -1,12 +1,28 @@
 import { useState, useEffect, useRef } from "react";
 import { useAppContext } from "../../services/AppContext";
 import { useSocketContext } from "../../services/SocketContext";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import Button from '@mui/material/Button';
 
 const SwitchOnOff = (props) => {
   const [switchStatus, setSwitchStatus] = useState(false);
   const socketCtx = useSocketContext();
   const appCtx = useAppContext();
   const tempSwitch = useRef()
+
+
+  const theme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        light: '#01bd7d',
+        main: '#01bd7d',
+        dark: '#01bd7d',
+        contrastText: '#fff',
+      },
+    }
+  })
 
   const switchFunction = () => {
     socketCtx.socket.on("status", payload => {
@@ -23,10 +39,12 @@ const SwitchOnOff = (props) => {
 
   const switch_Handler = (event) => {
     event.preventDefault();
+    //event.target.textContent = 'On';
+
     const newStatus = !switchStatus;
     socketCtx.socket.emit("command", {
       userId: socketCtx.getNewUsername(),
-      componentId:'Michelson_laser_power',
+      componentId: 'Michelson_laser_power',
       command: {
         controlId: 'switch',
         val: newStatus
@@ -39,8 +57,10 @@ const SwitchOnOff = (props) => {
 
   return (
     <div className="switchOnOff">
-      <button onClick={switch_Handler}>
-        {switchStatus ? 'ON' : 'OFF'}</button>
+      <ThemeProvider theme={theme}>
+        <Button variant="contained" onClick={switch_Handler} sx={{ width: '60px', padding: "10px", marginLeft: "20px", marginTop: "20px" }}>
+          {switchStatus ? 'ON' : 'OFF'}</Button>
+      </ThemeProvider>
     </div>
   )
 }
