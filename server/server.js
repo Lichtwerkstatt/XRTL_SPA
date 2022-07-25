f8c46fcfffabb2dd12c4141cea205bcb215ef7b6
Claralein <93766288+Claralein@users.noreply.github.com>
2022-07-25 13:46:34 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,6 @@ var footerList = [];
 var componentID = '';
 const socketToRoom = {};
 var GUIId = ""
-var online = false;
 var footerStatus = "Connected!"
 
 
@@ -188,7 +187,7 @@ io.on('connection', socket => {
         console.log(componentList.includes(payload))
         if (footerList.includes(payload) === true) {
             var statusFoot = footerList.indexOf(payload);
-            online = componentList.includes(payload);
+            var online = componentList.includes(payload);
             footerStatus = footerList[statusFoot + 1]
         }
         console.log("liste ", componentList.includes(payload))
