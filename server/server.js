a373a22ac1eec848caaf89a2ca3c67d1d503b24d
Claralein <93766288+Claralein@users.noreply.github.com>
2022-09-27 13:54:45 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const app = require('express')();
-const server = require('https').createServer(app);
+const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
         origin: '*',
