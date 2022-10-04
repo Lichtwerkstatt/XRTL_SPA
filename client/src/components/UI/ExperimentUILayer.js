83257d5dc79fd984bece9bdff4e5c33a7c4ec687
Claralein <clara.henkel@uni-jena.de>
2022-10-04 14:23:49 +0200

    Console.log deleted

--- client/src/components/UI/ExperimentUILayer.js
+++ client/src/components/UI/ExperimentUILayer.js
@@ -14,7 +14,6 @@ const ExperimentUILayer = () => {
 
   const ConnectionSuccess = () => {
     socketCtx.socket.on('Auth', () => {
-      console.log("hier")
       popupCtx.toggleShowPopUp('Connection successful!', 'success');
       setConnection(true);
     });
