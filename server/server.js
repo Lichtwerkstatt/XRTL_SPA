5f0f0279362f71c384f5f6ff49c29a8806728544
Claralein <93766288+Claralein@users.noreply.github.com>
2022-10-04 14:40:23 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
         origin: '*',
+         methods: ["GET", "POST"]
     }
 })
 const { v4: uuidv4 } = require('uuid');
