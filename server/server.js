ca9833c476ab120fea0019f190a63d26cbc4385f
Claralein <clara.henkel@uni-jena.de>
2022-10-19 14:17:36 +0200

    Update server

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const server = https.createServer({
 }, app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: ['http://localhost:3000', 'http://10.140.79.48:3000', 'https://lichtwerkstatt.github.io/XRTL_SPA'],
+        origin: ['https://lichtwerkstatt.github.io', 'http://localhost:3000', 'http://10.232.37.40:7000'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true
     }
