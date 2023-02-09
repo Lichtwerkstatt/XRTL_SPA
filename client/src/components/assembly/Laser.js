b4c38307ee24f0ab791357f0554186bbbf0aec32
Claralein <clara.henkel@uni-jena.de>
2023-02-09 08:55:26 +0100

    handleClose

--- client/src/components/assembly/Laser.js
+++ client/src/components/assembly/Laser.js
@@ -16,7 +16,7 @@ const Laser = (props) => {
   const popupCtx = usePopUpContext();
 
   const handleCloseWindow = () => {
-    appCtx.toggleSelectedComp(props.controlId)
+    appCtx.toggleSelectedComp(props.id)
   }
 
   const handleChangeFooter = (newFooter) => {
