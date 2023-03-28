86ff3cf66b372f77e510021ec690a8bd6a9d6637
Claralein <clara.henkel@uni-jena.de>
2023-03-28 12:56:21 +0200

    InfoWindow edit 2

--- client/src/components/windows/InfoWindow.js
+++ client/src/components/windows/InfoWindow.js
@@ -1,14 +1,7 @@
-import { useAppContext } from "../../services/AppContext";
 import Window from "../UI/experimentUI/Window";
 import InfoWindowContent from "./InfoWindowContent";
 
 const InfoWindow = (props) => {
-    const appCtx = useAppContext();
-
-    const handleCloseWindow = () => {
-        appCtx.toggleShowInfoWindow();
-    };
-
     return (
         <Window
             header="About XR TwinLab"
