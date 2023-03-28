7052d7b677cdb64a5214b58fdb6771d72f64274e
Claralein <clara.henkel@uni-jena.de>
2023-03-28 14:47:57 +0200

    NavBar edit

--- client/src/components/UI/experimentUI/NavBar.js
+++ client/src/components/UI/experimentUI/NavBar.js
@@ -1,16 +1,18 @@
-
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 import { MdOutlineScreenRotation, MdInfoOutline } from 'react-icons/md'
 import { useSocketContext } from '../../../services/SocketContext'
 import { useAppContext } from '../../../services/AppContext'
 import { GiLaserWarning } from 'react-icons/gi'
+import { MenuItem, Menu, ThemeProvider, IconButton, Tooltip } from '@mui/material';
 import { ImConnection } from 'react-icons/im'
 import styles from '../CSS/NavBar.module.css'
-import Tooltip from '@mui/material/Tooltip'
 import { BsCamera } from 'react-icons/bs'
 import { FaTags } from 'react-icons/fa'
 //import { BsBox } from 'react-icons/bs'
+import { Icon } from '@iconify/react';
+import { memo, Fragment } from 'react'
 import { isEqual } from 'lodash';
-import { memo } from 'react'
+import { theme } from './../templates/Theme'
 
 const NavBar = () => {
     const appCtx = useAppContext();
@@ -24,26 +26,91 @@ const NavBar = () => {
     if (appCtx.showTags) { showTagsColor = 'white' }
     let cameraStatusColor = '';
     if (appCtx.showCam) { cameraStatusColor = 'white' }
-    let showBeamColor = '';
-    if (appCtx.showBeam) { showBeamColor = 'white' }
     let showInfoWindowColor = '';
     if (appCtx.showInfoWindow) { showInfoWindowColor = 'white' }
 
     return <div id='navbar' className={styles.navbar} >
         <h1>XR TwinLab</h1>
+        <div className={styles.navMenuLaser}>
+            <h3>Overlay:</h3>
+            <ThemeProvider theme={theme} >
+                <PopupState variant="popover" popupId="demo-popup-menu">
+                    {(popupState) => (
+                        <Fragment>
+                            <IconButton variant="contained" {...bindTrigger(popupState)} sx={{
+                                borderRadius: 1,
+                                color: 'black',
+                                ':hover': {
+                                    bgcolor: 'darkgreen',
+                                    color: '#00ffa8',
+                                },
+                                marginTop: '-2%',
+                                paddingLeft: 20
+
+                            }}>
+                                <Icon icon="mdi:led-off" width="27" height="27" />
+                            </IconButton>
+                            <Menu {...bindMenu(popupState)}  >
+                                <MenuItem onClick={() => {
+                                    popupState.close();
+                                    appCtx.toggleShowLED('none');
+                                }} >None</MenuItem>
+                                <MenuItem onClick={() => {
+                                    popupState.close();
+                                    appCtx.toggleShowLED('white');
+                                }}>White</MenuItem>
+                                <MenuItem onClick={() => {
+                                    popupState.close();
+                                    appCtx.toggleShowLED('red');
+                                }}>Red</MenuItem>
+                            </Menu>
+                        </Fragment>
+                    )}
+                </PopupState>
+                <PopupState variant="popover" popupId="demo-popup-menu">
+                    {(popupState) => (
+                        <Fragment>
+                            <IconButton variant="contained" size='25' sx={{
+                                borderRadius: 1,
+                                color: 'black',
+                                ':hover': {
+                                    bgcolor: 'darkgreen',
+                                    color: '#00ffa8',
+                                },
+                                marginTop: '-1%',
+                                paddingLeft: 20,
+                                margin: 'auto'
+                            }}  {...bindTrigger(popupState)}>
+                                <GiLaserWarning />
+                            </IconButton>
+                            <Menu {...bindMenu(popupState)}>
+                                <MenuItem onClick={() => {
+                                    popupState.close();
+                                    appCtx.toggleShowBeam('on');
+                                }} >On</MenuItem>
+                                <MenuItem onClick={() => {
+                                    popupState.close();
+                                    appCtx.toggleShowBeam('off');
+                                }}>Off</MenuItem>
+                                <MenuItem onClick={() => {
+                                    popupState.close();
+                                    appCtx.toggleShowBeam('split');
+                                }}>Beamsplitter</MenuItem>
+                            </Menu>
+                        </Fragment>
+                    )}
+                </PopupState>
+            </ThemeProvider>
+        </div>
         <div className={styles.navMenu}>
             <ul>
                 <Tooltip title='Connnection'>
                     <li onClick={() => { (socketCtx.connected) ? socketCtx.toggleConnection() : appCtx.toggleLogin(); }}><ImConnection size={29} color={connectionStatusColor} /></li>
                 </Tooltip>
 
-           {/*      <Tooltip title='2D model'>
+                {/*                 <Tooltip title='2D model'>
                     <li onClick={appCtx.toggleShowVirtualLayer}><BsBox size={26} /></li>
                 </Tooltip> */}
-
-                <Tooltip title='Laser beam'>
-                    <li onClick={appCtx.toggleShowBeam}><GiLaserWarning size={29} color={showBeamColor} /></li>
-                </Tooltip>
 
                 <Tooltip title='Rotation'>
                     <li onClick={appCtx.toggleAutoRotate}><MdOutlineScreenRotation size={26} color={autoRotateColor} /></li>
