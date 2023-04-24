5dc63ae7d133eb529faee183557105becdad28bf
Claralein <clara.henkel@uni-jena.de>
2023-04-24 10:53:28 +0200

    NavBar fix

--- client/src/components/UI/experimentUI/NavBar.js
+++ client/src/components/UI/experimentUI/NavBar.js
@@ -1,18 +1,14 @@
-import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
-import { MdOutlineScreenRotation, MdInfoOutline, MdOutlineHelp, MdOutlineMenuBook } from 'react-icons/md'
+import { MdOutlineScreenRotation, MdInfoOutline, MdOutlineHelp, MdOutlineMenuBook } from 'react-icons/md';
 import { MenuItem, Menu, ThemeProvider, IconButton, Tooltip } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { useSocketContext } from '../../../services/SocketContext'
 import { useAppContext } from '../../../services/AppContext'
-import { GiLaserWarning } from 'react-icons/gi'
 import { ImConnection } from 'react-icons/im'
 import styles from '../CSS/NavBar.module.css'
 import { theme } from './../templates/Theme'
 import { BsCamera } from 'react-icons/bs'
-import { FaTags } from 'react-icons/fa'
-//import { BsBox } from 'react-icons/bs'
-import { Icon } from '@iconify/react';
-import { memo, Fragment, useState } from 'react'
+import { FaTags } from 'react-icons/fa';
+import { memo, useState } from 'react';
 import { isEqual } from 'lodash';
 
 const NavBar = () => {
@@ -37,156 +33,51 @@ const NavBar = () => {
     const [mobileVersion, setMobileVersion] = useState(null);
     const openMobileVersion = Boolean(mobileVersion);
 
-    const [led, setLED] = useState(null);
-    const openLED = Boolean(led)
-
-    const [laserBeam, setLaserBeam] = useState(null);
-    const openLaserBeam = Boolean(laserBeam)
-
-
     const handleClick = (event) => {
         setMobileVersion(event.currentTarget);
-    };
-
-    const handleClick2 = (event) => {
-        setLED(event.currentTarget);
-    };
-
-    const handleLaserBeam = (event) => {
-        setLaserBeam(event.currentTarget);
     };
 
     const handleMobileVersion = () => {
         setMobileVersion(null);
     };
-    const handleLED = () => {
-        setLED(null);
-    };
-
-    const closeLaserBeam = () => {
-        setLaserBeam(null);
-    };
 
     return (
-
         <div id='navbar' className={styles.navbar} >
-            <h1>XR TwinLab</h1>
-            <div className={styles.navMenuLaser}>
-                <h3>Overlay:</h3>
-                <ThemeProvider theme={theme} >
+            <ThemeProvider theme={theme} >
+                <h1>XR TwinLab</h1>
 
-                    <IconButton onClick={handleClick2} variant="contained" sx={{
-                        borderRadius: 1,
-                        height: '33px',
-                        width: '30px',
-                        color: 'black',
-                        ':hover': {
-                            bgcolor: 'darkgreen',
-                            color: '#00ffa8',
-                        },
-                    }}>
-                        <Icon icon="mdi:led-off" width="70" height="70" />
-                    </IconButton>
+                <div className={styles.navMenu}>
+                    <ul>
+                        <Tooltip title='Connnection'>
+                            <li onClick={() => { (socketCtx.connected) ? socketCtx.toggleConnection() : appCtx.toggleLogin(); }}><ImConnection size={29} color={connectionStatusColor} /></li>
+                        </Tooltip>
 
-                    <Menu
-                        id="demo-customized-menu"
-                        MenuListProps={{
-                            'aria-labelledby': 'demo-customized-button',
-                        }}
-                        mobileVersion={led}
-                        open={openLED}
-                        onClose={handleLED}
-                    >
-                        <MenuItem onClick={() => {
-                            handleLED();
-                            appCtx.toggleShowBeam('none');
-                        }} disableRipple >None</MenuItem>
-                        <MenuItem onClick={() => {
-                            handleLED();
-                            appCtx.toggleShowBeam('white');
-                        }} disableRipple>White</MenuItem>
-                        <MenuItem onClick={() => {
-                            handleLED();
-                            appCtx.toggleShowBeam('red');
-                        }} disableRipple>Red</MenuItem>
-                    </Menu>
+                        <Tooltip title='Rotation'>
+                            <li onClick={appCtx.toggleAutoRotate}><MdOutlineScreenRotation size={26} color={autoRotateColor} /></li>
+                        </Tooltip>
 
-                    <IconButton onClick={handleLaserBeam} variant="contained" sx={{
-                        borderRadius: 1,
-                        height: '33px',
-                        width: '30px',
-                        color: 'black',
-                        ':hover': {
-                            bgcolor: 'darkgreen',
-                            color: '#00ffa8',
-                        },
-                    }}>
-                        <GiLaserWarning color={'white'} />
-                    </IconButton>
+                        <Tooltip title='Labels'>
+                            <li onClick={appCtx.toggleShowTags}><FaTags size={25} color={showTagsColor} /></li>
+                        </Tooltip>
 
-                    <Menu
-                        id="demo-customized-menu"
-                        MenuListProps={{
-                            'aria-labelledby': 'demo-customized-button',
-                        }}
-                        mobileVersion={laserBeam}
-                        open={openLaserBeam}
-                        onClose={closeLaserBeam}
-                    >
-                        <MenuItem onClick={() => {
-                            closeLaserBeam();
-                            appCtx.toggleShowBeam('on');
-                        }} disableRipple >On</MenuItem>
-                        <MenuItem onClick={() => {
-                            closeLaserBeam();
-                            appCtx.toggleShowBeam('off');
-                        }} disableRipple>Off</MenuItem>
-                        <MenuItem onClick={() => {
-                            closeLaserBeam();
-                            appCtx.toggleShowBeam('split');
-                        }} disableRipple>Beamsplitter</MenuItem>
-                    </Menu>
+                        <Tooltip title='Webcam'>
+                            <li onClick={appCtx.toggleCam}><BsCamera size={26} color={cameraStatusColor} /></li>
+                        </Tooltip>
 
-                </ThemeProvider>
-            </div>
-            <div className={styles.navMenu}>
-                <ul>
-                    <Tooltip title='Connnection'>
-                        <li onClick={() => { (socketCtx.connected) ? socketCtx.toggleConnection() : appCtx.toggleLogin(); }}><ImConnection size={29} color={connectionStatusColor} /></li>
-                    </Tooltip>
+                        <Tooltip title='Manual'>
+                            <li onClick={appCtx.toggleShowManualWindow}><MdOutlineMenuBook size={26} color={showManualWindowColor} /></li>
+                        </Tooltip>
 
-                    {/*                 <Tooltip title='2D model'>
-                    <li onClick={appCtx.toggleShowVirtualLayer}><BsBox size={26} /></li>
-                </Tooltip> */}
+                        <Tooltip title='Help'>
+                            <li onClick={appCtx.toggleShowHelpWindow}><MdOutlineHelp size={26} color={showHelpWindowColor} /></li>
+                        </Tooltip>
 
-                    <Tooltip title='Rotation'>
-                        <li onClick={appCtx.toggleAutoRotate}><MdOutlineScreenRotation size={26} color={autoRotateColor} /></li>
-                    </Tooltip>
+                        <Tooltip title='Information'>
+                            <li onClick={appCtx.toggleShowInfoWindow}><MdInfoOutline size={26} color={showInfoWindowColor} /></li>
+                        </Tooltip>
+                    </ul>
+                </div>
 
-                    <Tooltip title='Labels'>
-                        <li onClick={appCtx.toggleShowTags}><FaTags size={25} color={showTagsColor} /></li>
-                    </Tooltip>
-
-                    <Tooltip title='Webcam'>
-                        <li onClick={appCtx.toggleCam}><BsCamera size={26} color={cameraStatusColor} /></li>
-                    </Tooltip>
-
-                    <Tooltip title='Manual'>
-                        <li onClick={appCtx.toggleShowManualWindow}><MdOutlineMenuBook size={26} color={showManualWindowColor} /></li>
-                    </Tooltip>
-
-                    <Tooltip title='Help'>
-                        <li onClick={appCtx.toggleShowHelpWindow}><MdOutlineHelp size={26} color={showHelpWindowColor} /></li>
-                    </Tooltip>
-
-                    <Tooltip title='Information'>
-                        <li onClick={appCtx.toggleShowInfoWindow}><MdInfoOutline size={26} color={showInfoWindowColor} /></li>
-                    </Tooltip>
-
-                </ul>
-            </div>
-
-            <ThemeProvider theme={theme}>
                 <div className={styles.mobile}>
                     <IconButton onClick={handleClick} variant="contained" sx={{
                         borderRadius: 1,
@@ -206,7 +97,7 @@ const NavBar = () => {
                         MenuListProps={{
                             'aria-labelledby': 'demo-customized-button',
                         }}
-                        mobileVersion={mobileVersion}
+                        anchorEl={mobileVersion}
                         open={openMobileVersion}
                         onClose={handleMobileVersion}
                     >
