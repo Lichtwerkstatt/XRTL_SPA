4075e966173b27fccae4b03ef5e753849db8b0e2
Claralein <clara.henkel@uni-jena.de>
2023-05-09 12:14:57 +0200

    Manual & welcome window are responsive now

--- client/src/components/windows/ManualWindow.js
+++ client/src/components/windows/ManualWindow.js
@@ -2,14 +2,33 @@ import Window from "../UI/experimentUI/Window";
 import ManualWindowContent from "./Content/ManualWindowContent";
 
 const ManualWindow = (props) => {
+    var width = window.innerWidth
+    var height = 0;
+
+    if (0 < width && width < 576) {
+        width = '350px'
+        height = '480px'
+    }
+    else if (576 < width && width < 768) {
+        width = '510px'
+        height = '440px'
+    }
+    else if (768 < width && width < 1000) {
+        width = '510px'
+        height = '440px'
+    } else {
+        width = '510px'
+        height = '440px'
+    }
+
     return (
         <Window
             header="XR TwinLab Manual"
             id='manual'
             top="100"
             left="200"
-            width="300"
-            height="547"
+            width={width}
+            height={height}
             footer={'empty'}
             topper={'none'}
         >
--- client/src/components/windows/WelcomeWindow.js
+++ client/src/components/windows/WelcomeWindow.js
@@ -2,14 +2,33 @@ import Window from "../UI/experimentUI/Window";
 import WelcomeWindowContent from "./Content/WelcomeWindowContent";
 
 const WelcomeWindow = (props) => {
+    var width = window.innerWidth
+    var height = 0;
+
+    if (0 < width && width < 576) {
+        width = '350px'
+        height = '480px'
+    }
+    else if (576 < width && width < 768) {
+        width = '510px'
+        height = '440px'
+    }
+    else if (768 < width && width < 1000) {
+        width = '510px'
+        height = '440px'
+    } else {
+        width = '510px'
+        height = '440px'
+    }
+
     return (
         <Window
             header="XR TwinLab Welcome"
             id='welcome'
             top="100"
             left="200"
-            width="300"
-            height="547"
+            width={width}
+            height={height}
             footer={'empty'}
             topper={'none'}
         >
