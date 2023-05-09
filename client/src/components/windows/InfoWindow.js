97c53b82ef052f3781158b2f991e7cc1b45cdc28
Claralein <clara.henkel@uni-jena.de>
2023-05-09 11:27:11 +0200

    Optimization of the info window (mobile view)

--- client/src/components/windows/InfoWindow.js
+++ client/src/components/windows/InfoWindow.js
@@ -2,14 +2,33 @@ import Window from "../UI/experimentUI/Window";
 import InfoWindowContent from "./Content/InfoWindowContent";
 
 const InfoWindow = (props) => {
+    var width = window.innerWidth
+    var height = 0;
+
+    if (0 < width && width < 576) {
+        width = '350px'
+        height = '545px'
+    }
+    else if (576 < width && width < 768) {
+        width = '600px'
+        height = '480px'
+    }
+    else if (768 < width && width < 1000) {
+        width = '600px'
+        height = '510px'
+    } else {
+        width = '600px'
+        height = '500px'
+    }
+
     return (
         <Window
             header="About XR TwinLab"
             id='info'
             top="250"
             left="200"
-            width="600px"
-            height='500px'
+            width={width}
+            height={height}
             footer={'empty'}
             topper={'para'}
         >
