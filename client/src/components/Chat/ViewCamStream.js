739d20bb0a4c3b8bc9c46dda6a05c0612138b074
Claralein <clara.henkel@uni-jena.de>
2023-05-15 10:32:25 +0200

    Cam responsive to size

--- client/src/components/Chat/ViewCamStream.js
+++ client/src/components/Chat/ViewCamStream.js
@@ -1,31 +1,32 @@
 import { useSocketContext } from '../../services/SocketContext'
 import { useEffect } from 'react';
+import styles from './CSS/ViewCam.module.css'
 
 const ViewCam = (props) => {
     const socketCtx = useSocketContext();
 
     useEffect(() => {
         var peerConnection;
-        
+
         const offer = (payload) => {
             peerConnection = props.peer;
             peerConnection
-            .setRemoteDescription(payload.data)
-            .then(() => peerConnection.createAnswer())
-            .then((sdp) => peerConnection.setLocalDescription(sdp))
-            .then(() => socketCtx.socket.emit('answer', { id: payload.id, data: peerConnection.localDescription }))
-            
+                .setRemoteDescription(payload.data)
+                .then(() => peerConnection.createAnswer())
+                .then((sdp) => peerConnection.setLocalDescription(sdp))
+                .then(() => socketCtx.socket.emit('answer', { id: payload.id, data: peerConnection.localDescription }))
+
             peerConnection.ontrack = (event) => {
                 document.getElementById('video').srcObject = event.streams[0];
             }
-            
+
             peerConnection.onicecandidate = (event) => {
                 if (event.candidate) {
                     socketCtx.socket.emit('candidate', { id: payload.id, data: event.candidate })
                 }
             }
         }
-        
+
         const candidate = (payload) => {
             peerConnection
                 .addIceCandidate(new RTCIceCandidate(payload.data))
@@ -48,9 +49,10 @@ const ViewCam = (props) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    // width="640px" height="480px"
     return (
-        <div>
-            <video className="video" id='video' autoPlay playsInline width="640px" height="480px" ></video>
+        <div className={styles.mainWrapper}>
+            <video className="video" id='video' autoPlay playsInline ></video>
         </div>
     );
 };
--- client/src/components/windows/CamWindow.js
+++ client/src/components/windows/CamWindow.js
@@ -4,6 +4,25 @@ import ViewCam from "../Chat/ViewCamStream";
 const InfoWindow = (props) => {
     const config = { iceServers: [{ urls: ["stun:stun.l.google.com:19302"] }] } //stun:stun.stunprotocol.org
     var peerConnection = new RTCPeerConnection(config);
+    var width = window.innerWidth
+    var height = 0;
+
+    if (0 < width && width < 576) {
+        width = '370px'
+        height = '278px'
+    }
+    else if (576 < width && width < 768) {
+        width = '500px'
+        height = '375px'
+    }
+    else if (768 < width && width < 1000) {
+        width = '600px'
+        height = '450px'
+    } else {
+        width = '750px'
+        height = '563px'
+    }
+
 
     return (
         <Window
@@ -12,7 +31,8 @@ const InfoWindow = (props) => {
             left="250"
             title="Cam_1"
             id="Cam_1"
-            width="600px"
+            width={width}
+            height={height}
             footer={'empty'}
             topper={'none'}
         >
