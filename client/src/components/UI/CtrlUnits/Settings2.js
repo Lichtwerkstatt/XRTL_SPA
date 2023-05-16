5f4618e8d4c465508ee30b9a22ebe7ec1050f19b
Claralein <clara.henkel@uni-jena.de>
2023-05-16 09:21:55 +0200

    Little changes

--- client/src/components/UI/CtrlUnits/Settings2.js
+++ client/src/components/UI/CtrlUnits/Settings2.js
@@ -13,6 +13,7 @@ const Settings = (props) => {
 
         if (props.width === '700px') {
             document.getElementById('Canvas').style.left = '-340px'
+            document.getElementById('Canvas').style.transform = 'rotate(180deg)'
         }
 
         const status = (payload) => {
