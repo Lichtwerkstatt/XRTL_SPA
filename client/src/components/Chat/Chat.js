d10deee4ae3cb0fd4bd5657322e9c9ed463aef45
Claralein <clara.henkel@uni-jena.de>
2023-06-06 09:14:03 +0200

    Chat update

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,4 +1,5 @@
 import { useSocketContext } from '../../services/SocketContext';
+import { useAppContext } from '../../services/AppContext';
 import { theme } from '../../components/UI/templates/Theme';
 import { useEffect, useState, memo } from 'react'
 import styles from './CSS/Chat.module.css'
@@ -9,9 +10,11 @@ import { ThemeProvider, InputAdornment, IconButton, FormControl, InputLabel, Out
 const Chat = () => {
   const [showChat, setShowChat] = useState(false);
   const [animation, setAnimation] = useState('');
-  const [message, setMessage] = useState('');
+  var [message, setMessage] = useState('');
   const [chat, setChat] = useState([]);
+
   const socketCtx = useSocketContext();
+  const appCtx = useAppContext();
 
   useEffect(() => {
     const message = (payload) => {
@@ -27,7 +30,76 @@ const Chat = () => {
 
   const sendMessage = (event) => {
     event.preventDefault();
-    socketCtx.socket.emit('message', { userId: socketCtx.username, message: message, color: socketCtx.fontColor });
+    if (message.at(0) === '!') {
+
+      // console.log('message', message)
+
+      if (message === '!rotate' || message === '!r') {
+        appCtx.toggleAutoRotate();
+        setChat([...chat, { userId: 'XRTL', message: 'HRotation command was sent ... ', color: '#FF7373' }]);
+      } else if (message === '!constructiom' || message === '!c') {
+        appCtx.toggleunderConstruction();
+      }
+      else if (message === '!user' || message === '!users') {
+
+        socketCtx.socket.emit('updateUser')
+
+        socketCtx.socket.on('updateUser', (payload) => {
+          //console.log(payload)
+          var user = ''
+          for (var i = 2; i < payload.length; i += 3) {
+            user += payload[i] + ','
+          }
+          user = user.slice(0, -1)
+          setChat([...chat, { userId: 'XRTL', message: 'List of all the active user/s: ' + user, color: '#FF7373' }]);
+        })
+      }
+      else if (message === '!reset') {
+        socketCtx.socket.emit('message', { userId: 'XRTL', message: 'Attention the reset command was emited!', color: '#FF7373' });
+
+        const controlIds = ['KM100_top_1', 'KM100_bottom_1', 'linear_1', 'greenlaser_top_1', 'greenlaser_bottom_1', 'beamSplitter']
+
+        for (var i = 0; i < controlIds.length; i++) {
+          socketCtx.socket.emit('command', {
+            userId: socketCtx.username,
+            controlId: controlIds[i],
+            reset: true
+          })
+        }
+      }
+      else if (message === '!showcase' || message === '!s') {
+        // socketCtx.socket.emit ('updateUser') 
+      }
+      else if (message === '!cam') {
+        socketCtx.socket.emit("command", {
+          userId: 'XRTL',
+          controlId: 'overview',
+          frameSize: 10
+        })
+
+        socketCtx.socket.emit("command", {
+          userId: 'XRTL',
+          controlId: 'overview',
+          exposure: 800,
+          color: socketCtx.fontColor,
+        })
+
+        socketCtx.socket.emit("command", {
+          userId: 'XRTL',
+          controlId: 'overview',
+          contrast: 1,
+          color: socketCtx.fontColor,
+        })
+
+        setChat([...chat, { userId: 'XRTL', message: 'The highest camera settings have been made!', color: '#FF7373' }]);
+      }
+      else {
+        setChat([...chat, { userId: 'XRTL', message: "Command doesn't exists", color: '#FF7373' }]);
+      }
+
+    } else {
+      socketCtx.socket.emit('message', { userId: socketCtx.username, message: message, color: socketCtx.fontColor });
+    }
     setMessage('');
   }
 
