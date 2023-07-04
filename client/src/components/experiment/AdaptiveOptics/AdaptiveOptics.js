c334bd11195aae7cf079b1749541ec96105bc1b4
Claralein <clara.henkel@uni-jena.de>
2023-07-04 14:14:14 +0200

    changes

--- client/src/components/experiment/AdaptiveOptics/AdaptiveOptics.js
+++ client/src/components/experiment/AdaptiveOptics/AdaptiveOptics.js
@@ -3,23 +3,10 @@ import LaserCtrl from "../../assembly/Laser";
 import Rotary from '../../assembly/Rotary';
 import KM100 from "../../assembly/KM100";
 import SM1ZP from "../../assembly/SM1ZP";
-import { useState } from "react";
 
 const AdaptiveOptics = (props) => {
   let footer = "Initializing..."
   var zero = "0"
-  const [setting, setSetting] = useState(false)
-
-  var height = window.innerHeight;
-  var width = window.innerWidth;
-  var halfWidth = width / 2;
-  var topHigh = String(height - (height / 3 * 2.7))
-  var topHighMiddle = String(height - (height / 1.4))
-  var topMiddle = String(height - (height / 2.15))
-  var leftLeft = String(width - (halfWidth * 1.9))
-  var leftMiddle = String(width - (halfWidth * 1.8))
-  var leftRight = String(width - (halfWidth * 0.47))
-  var leftMiddleRight = String(width - (halfWidth * 0.4))
 
   return (
     <div>
