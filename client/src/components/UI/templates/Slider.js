76e644198569b63e86c2b05135ca8b8935a96fc1
Claralein <clara.henkel@uni-jena.de>
2023-07-12 10:12:15 +0200

    Slider docs update

--- client/src/components/UI/templates/Slider.js
+++ client/src/components/UI/templates/Slider.js
@@ -8,12 +8,14 @@ import { useState } from "react";
  * Slider component
  * 
  * @description Used to create a slider with a specified start and end point. In addition, the controlId, the status (of the component and the online status) 
- * and the command for the server must be specified. The title can but does not have to be given.
+ * and the command for the server must be specified. The title can but does not have to be given. To display text under the slider instead of numbers, this 
+ * can be passed to the slider within the tag text in the form of an array. 
  * 
  * @param {string} component - controlId 
  * @param {string} title - title 
  * @param {number} min -  text at the left/beginning
- * @param {number} max -text at the right/end
+ * @param {number} max - text at the right/end
+ * @param {array} text - contains an array with numbers as keys and strings as values (for displaying text under the slider instead of numbers)
  * @param {number} sliderValue - Status of the slider from the status request 
  * @param {boolean} online - connection status to the component
  * @param {string} option - command for the server
@@ -22,7 +24,7 @@ import { useState } from "react";
  * 
  * @example <Slider title='Contrast' component={'cam'} online={true} sliderValue={0} min={-2} max={2} option='contrast' />
  * @example <Slider title='Exposure' component={'cam'} online={false} sliderValue={-2} min={0} max={1200} option='exposure' />
- * @example <Slider component={'cam'} online={false} sliderValue={-2} min={0} max={1200} option='exposure' />
+ * @example <Slider component={'cam'} online={false} sliderValue={-2} min={0} max={10} text={[{ value: 0, label: 'a' }, { value: 5, label: 'b' }, { value: 10, label: 'c' }]} option='exposure' />
  */
 
 const SliderCtrl = (props) => {
@@ -119,6 +121,7 @@ SliderCtrl.propTypes = {
   title: propTypes.string,
   min: propTypes.number.isRequired,
   max: propTypes.number.isRequired,
+  text: propTypes.array,
   sliderValue: propTypes.bool.isRequired,
   online: propTypes.bool.isRequired,
   option: propTypes.string.isRequired
