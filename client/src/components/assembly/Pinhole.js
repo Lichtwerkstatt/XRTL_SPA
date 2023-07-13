ffc7a1e51954a8ca4aab0445fd6d61053d1fd65d
digiPHOTON-FL <fabian.lukas@uni-jena.de>
2023-07-13 15:45:44 +0200

    Pinhole

--- client/src/components/assembly/Pinhole.js
+++ client/src/components/assembly/Pinhole.js
@@ -1,25 +1,26 @@
-import BS_Background from './media/BS_outline.png';
-import PinholeCtrl from '../UI/CtrlUnits/SwitchCtrl';
-import Window from '../UI/experimentUI/Window';
+import PH_Background from "./media/id15_outline.png";
+import RotaryCtrl from "../UI/CtrlUnits/RotaryCtrl";
+import Window from "../UI/experimentUI/Window";
 
 const Pinhole = (props) => {
-
-    return (
-        <Window
-            id={props.controlId}
-            componentList={[props.controlId]}
-            header={props.title}
-            top={props.top}
-            left={props.left}
-            height='240px'
-            width='250px'
-            background={BS_Background}
-        >
-            <PinholeCtrl
-                component={props.controlId}
-            />
-
-        </Window>
-    )
+  return (
+    <Window
+      id={props.controlId}
+      componentList={[props.controlId]}
+      header={props.title}
+      top={props.top}
+      left={props.left}
+      height="240px"
+      width="250px"
+      background={PH_Background}
+    >
+      <RotaryCtrl
+        rotation={props.rotationTop}
+        component={props.controlIdTop}
+        top="20"
+        left="160"
+      />
+    </Window>
+  );
 };
 export default Pinhole;
