b57049db0a332de2cb855e0a21c75bec99319205
Claralein <clara.henkel@uni-jena.de>
2023-07-19 14:42:05 +0200

    ESPCam docs

--- client/src/components/assembly/ESPCamStream.js
+++ client/src/components/assembly/ESPCamStream.js
@@ -1,6 +1,20 @@
 import Settings from '../UI/CtrlUnits/ESPCamPlusSettings';
 import Window from '../UI/experimentUI/Window';
 
+/**
+ * ESPCam component window
+ * 
+ * @description This React component returns a window with the ESPCam stream and the settings for it.
+ * 
+ * @param {string} controlId - controlId
+ * @param {string} title - For setting the title within the window
+ * @param {number} top - For the positioning of the window 
+ * @param {number} left - For the positioning of the window 
+ * @param {boolean} setting - If the variable is set to true, the settings are displayed within the ESPCam window; if false, they are hidden.
+ * @param {function} setSettings(newVal) - Function to change the value of the settings varibale
+ * 
+ * @returns {React.ReactElement} ESPCam component window
+ */
 const ESPCamStream = (props) => {
   var width = window.innerWidth
   var height = 0;
