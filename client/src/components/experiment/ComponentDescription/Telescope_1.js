fe7d15619a82ea0c07ffae675b5d5f5699997e1b
Claralein <clara.henkel@uni-jena.de>
2023-08-22 11:14:21 +0200

    Telescope docs

--- client/src/components/experiment/ComponentDescription/Telescope_1.js
+++ client/src/components/experiment/ComponentDescription/Telescope_1.js
@@ -1,5 +1,15 @@
 import styles from './CSS/Description.module.css'
+import propTypes from "prop-types";
 
+/**
+ * Telescope 1 window content 
+ * 
+ * @description This file contains the text for the window of the first telescope, which can be seen within the component window when clicking on telescope 1.
+ * 
+ * @param {string} height - For scaling the content (is specified in pixels)
+ * 
+ * @returns {React.ReactElement} Content for the telescope 1 component window .  
+ */ 
 const DescriptionTelescope1 = (props) => {
     return (
         <div>
@@ -11,5 +21,9 @@ const DescriptionTelescope1 = (props) => {
 
         </div>
     )
+}
+
+DescriptionTelescope1.propTypes = {
+    height: propTypes.string.isRequired,
 }
 export default DescriptionTelescope1;
--- client/src/components/experiment/ComponentDescription/Telescope_2.js
+++ client/src/components/experiment/ComponentDescription/Telescope_2.js
@@ -1,5 +1,15 @@
 import styles from './CSS/Description.module.css'
+import propTypes from "prop-types";
 
+/**
+ * Telescope 2 window content 
+ * 
+ * @description This file contains the text for the window of the second telescope, which can be seen within the component window when clicking on telescope 2.
+ * 
+ * @param {string} height - For scaling the content (is specified in pixels)
+ * 
+ * @returns {React.ReactElement} Content for the telescope 2 component window .  
+ */
 const DescriptionTelescope2 = (props) => {
     return (
         <div>
@@ -11,5 +21,8 @@ const DescriptionTelescope2 = (props) => {
 
         </div>
     )
+}
+DescriptionTelescope2.propTypes = {
+    height: propTypes.string.isRequired,
 }
 export default DescriptionTelescope2;
