e90ab50a9608f4859738564af4ffeb101b592977
digiPHOTON-FL <fabian.lukas@uni-jena.de>
2023-08-23 10:23:43 +0200

    Eye renaming after km100 copy/paste

--- client/src/components/assembly/Eye.js
+++ client/src/components/assembly/Eye.js
@@ -1,24 +1,24 @@
-import Eye_Background from './media/Eye_outline.png'
-import RotaryCtrl from '../UI/CtrlUnits/RotaryCtrl';
-import Window from '../UI/experimentUI/Window';
+import Eye_Background from "./media/Eye_outline.png";
+import RotaryCtrl from "../UI/CtrlUnits/RotaryCtrl";
+import Window from "../UI/experimentUI/Window";
 
 /**
  * Eye component window
- * 
+ *
  * @description  This React component returns a window with the content for the eye component window.
- *  
+ *
  * @param {string} id - controlId of the entire component
- * @param {string} controlIdTop - controlId for the upper stepper motor 
+ * @param {string} controlIdTop - controlId for the upper stepper motor
  * @param {string} controlIdBottom - controlId for the lower stepper motor
  * @param {number} rotationTop - Initialisation value for upper control element
  * @param {number} rotationBottom - Initialisation value for lower control element
  * @param {string} title - For setting the title within the window
- * @param {number} top - For the positioning of the window 
- * @param {number} left - For the positioning of the window 
- * 
- * @returns {React.ReactElement} KM100 component window
+ * @param {number} top - For the positioning of the window
+ * @param {number} left - For the positioning of the window
+ *
+ * @returns {React.ReactElement} Eye component window
  */
-const KM100 = (props) => {
+const Eye = (props) => {
   return (
     <Window
       id={props.id}
@@ -26,23 +26,23 @@ const KM100 = (props) => {
       header={props.title}
       top={props.top}
       left={props.left}
-      height='240px'
-      width='250px'
+      height="240px"
+      width="250px"
       background={Eye_Background}
     >
       <RotaryCtrl
         rotation={props.rotationTop}
         component={props.controlIdTop}
-        top='15'
-        left='170'
+        top="15"
+        left="170"
       />
       <RotaryCtrl
         rotation={props.rotationBottom}
         component={props.controlIdBottom}
-        top='35'
-        left='170'
+        top="35"
+        left="170"
       />
     </Window>
   );
 };
-export default KM100;
+export default Eye;
