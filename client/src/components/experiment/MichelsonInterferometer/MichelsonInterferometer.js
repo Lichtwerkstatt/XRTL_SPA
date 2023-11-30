07067a80474a6870430bbca6677c542d4351ae69
Claralein <clara.henkel@uni-jena.de>
2023-11-30 10:23:16 +0100

    JS docs

--- client/src/components/experiment/MichelsonInterferometer/MichelsonInterferometer.js
+++ client/src/components/experiment/MichelsonInterferometer/MichelsonInterferometer.js
@@ -7,6 +7,30 @@ import Laser from '../../assembly/Laser';
 import KM100 from '../../assembly/KM100';
 import SM1ZP from '../../assembly/SM1ZP';
 
+
+/**
+ * Experiment Setup Component
+ *
+ * @description Within this React component, all experiment components are intialised and the renderings of their windows, if selected, are handled.
+ * In addition, important variables are passed to the individual components.
+ *
+ * @param {Set} selected - Set contains the controlIds of the component windows that are currently opened/rendered
+ * 
+ * @example 
+          {props.selected.has(// general controlId) && ( // handles the renderings of the window, if the variable selected includes the controlId 
+          <COMPONENT  
+          title= // Title within the topper of the component window
+          id={// general controlId of the component}
+          controlIdTop={// controlId for the upper stepper motor}
+          controlIdBottom={// controlId for the lower stepper motor}
+          footer={// Intialisation text of the footer}
+          top={// Positioning at the height of the window}
+          left={// Positioning at the width of the window}
+        />
+        )}
+ *
+ * @returns {React.ReactElement} Experiment setup component
+ */
 const MichelsonInterferometer = (props) => {
 
   return (
