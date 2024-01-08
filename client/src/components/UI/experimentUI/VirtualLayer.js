7bfff019409151b52e2369549fa767857a5df0e9
digiPHOTON-FL <fabian.lukas@uni-jena.de>
2024-01-08 14:05:56 +0100

    New and much cleaner 3D Model

--- client/src/components/UI/experimentUI/VirtualLayer.js
+++ client/src/components/UI/experimentUI/VirtualLayer.js
@@ -1,5 +1,5 @@
-import MI_230201 from "../../experiment/MichelsonInterferometer/MI_230201";
-import { useSocketContext } from '../../../services/SocketContext';
+import MI_240105 from "../../experiment/MichelsonInterferometer/MI_240105";
+import { useSocketContext } from "../../../services/SocketContext";
 import { OrbitControls, Environment } from "@react-three/drei";
 import { useAppContext } from "../../../services/AppContext";
 import { Canvas } from "@react-three/fiber";
@@ -9,24 +9,23 @@ const VirtualLayer = () => {
   const socketCtx = useSocketContext();
   const appCtx = useAppContext();
 
-
   if (appCtx.showVirtualLayer) {
     return (
       <Canvas
         style={{
-          position: 'absolute',
-          background: 'linear-gradient(Teal, Black)',
-          width: '100%',
-          height: '100%',
+          position: "absolute",
+          background: "linear-gradient(Teal, Black)",
+          width: "100%",
+          height: "100%",
         }}
         colorManagement
         softShadows
         camera={{ position: [0, 3, 5], fov: 40 }}
       >
         <Suspense fallback={null}>
-          <Environment files='../hdri/autoshop.hdr' />
+          <Environment files="../hdri/autoshop.hdr" />
           <OrbitControls autoRotate={appCtx.autoRotate} />
-          <MI_230201
+          <MI_240105
             toggleSelect={appCtx.toggleSelectedComp}
             selected={appCtx.selectedComps}
             showTags={appCtx.showTags}
