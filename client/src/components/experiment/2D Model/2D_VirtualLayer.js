a4f0cdff970c77f376067fbe0e9fe78621016694
FalkoFuroro <falko.sojka@googlemail.com>
2024-02-01 13:39:14 +0100

    Scaling and Coloring 2D-Hitboxes

--- client/src/components/experiment/2D Model/2D_VirtualLayer.js
+++ client/src/components/experiment/2D Model/2D_VirtualLayer.js
@@ -25,14 +25,32 @@ const Overlay = (props) => {
     let screen_width = window.innerWidth;
     let screen_height = window.innerHeight;
 
-    // Function scales the rectangular hitboxes according to the height and width of the browser window 
-    // Requires as input the coordinates of the box in the form of (x_start, y_end, x_end, y_end)
-    // Output: scaled array in the form (x_start, y_end, x_end, y_end)
-    const boxScaling = (arrayToScale) => {
-        let hitbox_scaling = [screen_width / 1920, screen_height / 955, screen_width / 1920, screen_height / 955];
-        arrayToScale = arrayToScale.map((value, index) => parseInt(value * hitbox_scaling[index]));
+    // The scaling and offset factors
+    let scaling = [1.0, 0.97]; // [x-scale, y-scale]
+    let offset = [0, 30]; // [x-offset, y-offset]
 
-        return arrayToScale;
+    // The Colors for the Hitboxes
+    let preFill_Color = "rgba(200, 200, 250, 0.2)";
+    let fill_Color = "rgba(200, 255, 200, 0.3)";
+
+    // Function scales the polygonal hitbox according to the height and width of the browser window 
+    // Requires an array in the format [x1, y1, x2, y2, ...]
+    // Output: scaled array in the form [x1, y1, x2, y2, ...]
+    const Strech_to_Screen = (arrayToTransform) => {
+        let hitbox_scaling = [screen_width / 1920, screen_height / 955];
+        arrayToTransform = arrayToTransform.map((value, index) => parseInt(value * (hitbox_scaling[index % hitbox_scaling.length])));
+
+        return arrayToTransform;
+    }
+
+    // Function transforms the polygonal hitbox coordinates according to scaling and offset factors
+    // Requires an array in the format [x1, y1, x2, y2, ...]
+    // Transforms the vector using the scaling and offset variables
+    const boxTransform = (arrayToTransform) => {
+        arrayToTransform = arrayToTransform.map((value, index) => parseInt(value * (scaling[index % scaling.length])));
+        arrayToTransform = arrayToTransform.map((value, index) => parseInt(value + (offset[index % offset.length])));
+
+        return arrayToTransform;
     }
 
     // Definition of all hitboxes by specifying the controlId, the shape, the coordinates (x_start, y_start, x_end, y_end) 
@@ -40,23 +58,23 @@ const Overlay = (props) => {
     let MAP = {
         name: "MI_Simple_imagemap",
         areas: [
-            { controlId: "KM100_1", shape: "rect", coords: boxScaling([450, 800, 740, 1000]), desc: 'Reference Mirror' },
-            { controlId: "linear_1", shape: "rect", coords: boxScaling([5, 480, 180, 740]), desc: 'Linear Movable Mirror' },
-            { controlId: "plate_rotation", shape: "rect", coords: boxScaling([200, 500, 300, 680]), desc: 'Glass Plate Rotation Stage' },
-            { controlId: "heater_rotation", shape: "rect", coords: boxScaling([680, 700, 900, 800]), desc: 'Mirror Changing Stage' },
-            { controlId: "greenlaser_1", shape: "rect", coords: boxScaling([1450, 400, 1700, 600]), desc: 'Laser Alignment' },
-            { controlId: "greenlaserPower_1", shape: "rect", coords: boxScaling([1350, 200, 1600, 450]), desc: 'Power Supply' },
-            { controlId: "screen", shape: "rect", coords: boxScaling([350, 0, 1050, 100]), desc: 'Screen' },
-            { controlId: "heater", shape: "rect", coords: boxScaling([730, 400, 940, 600]), desc: 'Retract. Beam Splitter' },
-            { controlId: "experimentSelection", shape: "rect", coords: boxScaling([0, 400, 50, 600]), desc: 'Multi Component Selection' },
-            { controlId: "lens", shape: "rect", coords: boxScaling([60, 400, 210, 600]), desc: 'Lens' },
-            { controlId: "bscube", shape: "rect", coords: boxScaling([220, 400, 370, 600]), desc: 'Beam Splitter' },
+            { controlId: "KM100_1", shape: "poly", coords: Strech_to_Screen(boxTransform([728,846, 945,846, 945,940, 900,940, 900,902, 770,902, 770,942, 727,942])), preFillColor: preFill_Color, fillColor: fill_Color, desc: 'Reference Mirror'},
+            { controlId: "linear_1", shape: "poly", coords: Strech_to_Screen(boxTransform([179,489, 217,494, 242,504, 259,524, 264,550, 247,581, 214,600, 179,609, 139,603, 114,592, 97,573, 90,548, 100,523, 112,504, 144,492])), preFillColor: preFill_Color, fillColor: fill_Color, desc: 'Linear Movable Mirror' },
+            { controlId: "plate_rotation", shape: "poly", coords: Strech_to_Screen(boxTransform([536,627, 744,577, 811,593, 833,638, 800,682, 578,726, 522,710, 508,665])), preFillColor: preFill_Color, fillColor: fill_Color, desc: 'Glass Plate Rotation Stage' },
+            { controlId: "heater_rotation", shape: "poly", coords: Strech_to_Screen(boxTransform([368,688, 422,672, 455,675, 635,785, 644,818, 618,860, 564,862, 360,763, 338,725])), preFillColor: preFill_Color, fillColor: fill_Color, desc: 'Mirror Changing Stage' },
+            { controlId: "greenlaser_1", shape: "poly", coords: Strech_to_Screen(boxTransform([1763,398, 1849,365, 1915,365, 1915,410, 1870,412, 1871,488, 1915,488, 1915,520, 1786,524])), preFillColor: preFill_Color, fillColor: fill_Color, desc: 'Laser Alignment' },
+            { controlId: "greenlaserPower_1", shape: "poly", coords: Strech_to_Screen(boxTransform([1577,725, 1777,725, 1789,865, 1585, 865])), preFillColor: preFill_Color, fillColor: fill_Color, desc: 'Power Supply' },
+            { controlId: "screen", shape: "poly", coords: Strech_to_Screen(boxTransform([516, 4, 1188, 4, 1140, 191, 647, 193])), preFillColor: preFill_Color, fillColor: fill_Color, desc: 'Screen' },
+            { controlId: "heater", shape: "poly", coords: Strech_to_Screen(boxTransform([5,415, 310,415, 310,487, 5,487])), preFillColor: preFill_Color, fillColor: fill_Color, desc: 'Retract. Beam Splitter' },
+            { controlId: "experimentSelection", shape: "poly", coords: Strech_to_Screen(boxTransform([984,395, 1078,394, 1078,257, 1104,257, 1104,490, 984,490])), preFillColor: preFill_Color, fillColor: fill_Color, desc: 'Multi Component Selection' },
+            { controlId: "lens", shape: "poly", coords: Strech_to_Screen(boxTransform([1200,393, 1282,392, 1284,501, 1200,501])), preFillColor: preFill_Color, fillColor: fill_Color, desc: 'Lens' },
+            { controlId: "bscube", shape: "poly", coords: Strech_to_Screen(boxTransform([772,390, 922,390, 933,408, 931,509, 783,511, 767,495])), preFillColor: preFill_Color, fillColor: fill_Color, desc: 'Beam Splitter' },
         ]
     };
 
     // Returns the position where the text to be displayed is to be placed
     const getTipPosition = (area) => {
-        return { top: `${area.center[1]}px`, left: `${area.center[0] - 20}px` };
+        return { top: `${area.center[1]-50}px`, left: `${area.center[0] - 50}px` };
     }
 
     // Adds the clicked component to the set within the AppContext, whereupon the corresponding component window is rendered.
@@ -98,8 +116,8 @@ const Overlay = (props) => {
                         onClick={(area) => clicked(area)}
                         onMouseEnter={(area) => enterArea(area)}
                         onMouseLeave={(area) => leaveArea(area)}
-                        lineWidth={2}
-                        strokeColor={"#01bd7d"}
+                        lineWidth={0}
+                        strokeColor={"rgba(200, 200, 200, 0.0)"}
                         width={window.innerWidth}
                         height={window.innerHeight}
                     />
